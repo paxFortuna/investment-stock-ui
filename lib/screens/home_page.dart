68c2c00db04ea0b6fa1b7155532328253c9dded3
fortuna <avidia38@gmail.com>
2022-08-16 19:35:23 +0900

    feat: renderflex overflow refined

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -21,68 +21,70 @@ class HomePage extends StatelessWidget {
               icon: const Icon(Icons.settings), onPressed: () => print("menu"))
         ],
       ),
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              color: const Color(0xFF020063),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height - 200,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Align(
-                alignment: Alignment.topCenter,
-                child: CardList(),
+      body: SingleChildScrollView(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 300,
+                color: const Color(0xFF020063),
               ),
-              LastInvestment(),
-              const Deposits(),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Investment()),
-                  );
-                },
-                child: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Color(0xFF24CCA7),
-                  ),
-                  width: MediaQuery.of(context).size.width - 50,
-                  height: 50,
-                  child: const Center(
-                      child: Text(
-                    "Go Invest",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600),
-                  )),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height - 200,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Align(
+                  alignment: Alignment.topCenter,
+                  child: CardList(),
                 ),
-              ),
-              const SizedBox(
-                height: 25,
-              )
-            ],
-          ),
-        ],
+                LastInvestment(),
+                const Deposits(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Investment()),
+                    );
+                  },
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color(0xFF24CCA7),
+                    ),
+                    width: MediaQuery.of(context).size.width - 50,
+                    height: 50,
+                    child: const Center(
+                        child: Text(
+                      "Go Invest",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600),
+                    )),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
